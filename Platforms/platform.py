1d911ff8ff44e80573e7294b6f74466eb1e3c100
Carlos Manuel <cmglezpdev@gmail.com>
2023-06-20 18:15:58 -0400

    fix: arguments of the Platform class

--- Platforms/platform.py
+++ Platforms/platform.py
@@ -2,21 +2,22 @@ from abc import ABC, abstractmethod
 
 class Platform (ABC):
     name: str
+    url_patterns: list[str]
 
     @abstractmethod
-    def is_valid_problem_url(self, url):
+    def is_valid_problem_url(self, url: str):
         pass
 
     @abstractmethod
-    def get_test_cases(self, url):
+    def get_test_cases(self, url: str):
         pass
 
     @abstractmethod
-    def get_problem_id(self, url):
+    def get_problem_id(self, url: str):
         pass
 
     @abstractmethod
-    def get_time_memory_limits(self, url):
+    def get_time_memory_limits(self, url: str):
         pass
 
     
